c1f2908a3ce731f43bd997a33d1150ece114a475
Jonahlee Xyrkcysz Fabula <xyrk.fabula@dice205.com>
2024-02-25 16:17:28 +0800

    feat: updated avatar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -39,7 +39,7 @@ export default class Navbar extends React.Component{
             <div className="dropdown dropdown-end">
               <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
-                  <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                  <img alt="Tailwind CSS Navbar component" src="./src/assets/avatar.png" />
                 </div>
               </div>
               <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
