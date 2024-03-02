067dd84adf1bd93c595a1336ef27a32d19bef9c6
Jonahlee Xyrkcysz Fabula <xyrk.fabula@dice205.com>
2024-03-02 20:33:48 +0800

    fix: image size and text in description

--- src/Components/Introduction/Description.jsx
+++ src/Components/Introduction/Description.jsx
@@ -7,7 +7,7 @@ export default class Description extends React.Component{
         return (
             <>
                 <div>
-                    <h1 className="mb-5 text-4xl font-bold text-justify text-white">Hi, I'm Jonahlee Xyrkcysz Fabula </h1>
+                    <h1 className="mb-5 text-[2.4em] font-bold text-justify text-white">Hi, I'm Jonahlee Xyrkcysz Fabula </h1>
                     <h1 className="mb-5 text-3xl text-left text-white">Software Developer / Engineer</h1>
                     <p className="flex-1 leading-normal text-justify text-white">
                         I currently work as TALL Stack and VueJS Developer but I am eager to study other technologies.
--- src/Components/Introduction/ProfileImage.jsx
+++ src/Components/Introduction/ProfileImage.jsx
@@ -5,9 +5,9 @@ export default class ProfileImage extends React.Component{
 
     render(){
         return (
-            <div className="flex justify-center align-middle w-fit">
+            <div className="flex justify-centeralign-middle w-fit">
                 <div className="rounded">
-                  <img className="" alt="Tailwind CSS Navbar component" src="./src/assets/avatar.png" />
+                  <img className="ml-32" alt="Tailwind CSS Navbar component" src="./src/assets/avatar.png" />
                 </div>
             </div>
         );
