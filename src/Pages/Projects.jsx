7073ba8ac56df63a4bfc2d22233bfb3e6fcc9dfa
Jonahlee Xyrkcysz Fabula <xyrk.fabula@dice205.com>
2024-03-03 21:15:53 +0800

    feat: move the assets to public folder

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -3,7 +3,7 @@ import ProjectsContainer from '../Components/ProjectsContainer';
 
 export default class Projects extends React.Component {
     render() {
-        const imagePath = "src/assets/bg-projects-page.svg";
+        const imagePath = "public/assets/bg-projects-page.svg";
         return (
             <div className="min-h-screen hero" style={{backgroundImage: 'url('+imagePath+')'}}>
             <div className="bg-opacity-60 hero-overlay"></div>
